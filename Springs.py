e831de5c8f9ee0d01a279c26957f0956ac730bc0
zxu32 <zhengxu32@gmail.com>
2016-10-11 23:25:57 -0400

    Spring

--- Springs.py
+++ Springs.py
@@ -4,31 +4,7 @@ import PyParticles
 import math
 
 
-class UniverseScreen:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        (self.dx, self.dy) = (0, 0)
-        (self.mx, self.my) = (0, 0)
-        self.magnification = 1
-
-    def scroll(self, dx=0, dy=0):
-        self.dx += (dx * self.width)/(self.magnification * 10)
-        self.dy += (dy * self.height)/(self.magnification * 10)
-
-    def zoom(self, zoom):
-        self.magnification *= zoom
-        self.mx = (1 - self.magnification) * self.width / 2
-        self.my = (1 - self.magnification) * self.height / 2
-
-    def reset(self):
-        (self.dx, self.dy) = (0, 0)
-        (self.mx, self.my) = (0, 0)
-        self.magnification = 1
-
 (w, h) = (400, 400)
-universeScreen = UniverseScreen(w, h)
-
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption('Springs')
 universe = PyParticles.Environment(w, h, color=(255, 255, 255))
@@ -68,15 +44,7 @@ while running:
         selected_particle.mouseMove(mouseX, mouseY)
     if not paused:
         universe.update()
+    for p in universe.particles:  # display particles from previous step
+        pygame.draw.circle(screen, p.color, (int(p.x), int(p.y)), p.size, p.thickness)
     screen.fill(universe.color)
-    for p in universe.particles:
-        x = int(universeScreen.mx + (universeScreen.dx + p.x) * universeScreen.magnification)
-        y = int(universeScreen.my + (p.y + universeScreen.dy) * universeScreen.magnification)
-        size = int(p.size * universeScreen.magnification)
-        if p.size < 2:
-            pygame.draw.rect(screen, p.color, (x, y, 2, 2))
-            # draw rectangle to approximate particle when they are too small
-        else:
-            pygame.draw.circle(screen, p.color, (x, y), int(p.size), 0)
-
     pygame.display.flip()
